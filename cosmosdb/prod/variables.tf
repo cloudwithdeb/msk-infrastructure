19e77bf7ac2d65c89f36bac553b4db21d943cf22
Owusu Bright Debrah <brightdebrah97@gmail.com>
2023-03-16 09:30:10 -0600

    updated cosmosdb.yml file

---
+++ cosmosdb/prod/variables.tf
@@ -0,0 +1,15 @@
+variable "subscription" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "database-name" {
+  type = string
+}
+
+variable "resource-group" {
+  type = string
+}
